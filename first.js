ed16ba45c70fdc801db10e8aeb1603d2022190ab
ivline11 <59316053+ivline11@users.noreply.github.com>
2023-03-04 17:18:41 +0900

    Add files via upload

---
+++ first.js
@@ -0,0 +1,8 @@
+//define an array of months
+const months = [
+	"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+
+function myFunction(){
+document.getElementById("demo").innerHTML = "second thing";
+}
